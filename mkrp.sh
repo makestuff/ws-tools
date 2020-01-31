945783d634d31201ce52a4381cc0b535eabfee18
Chris McClelland <prophet3636@gmail.com>
2020-01-31 11:03:42 +0000

    Fail if any of the tests failed

--- mkrp.sh
+++ mkrp.sh
@@ -3,3 +3,6 @@
 cd ${PROJ_HOME}
 cat $(find . -name "transcript" | sort) > transcripts.txt
 TRANSCRIPT=transcripts.txt python hdl-tools/ersatz-gtest.py --gtest_output=xml:svunit.xml
+if [ -e "${PROJ_HOME}/.testfail" ]; then
+    exit 1
+fi
